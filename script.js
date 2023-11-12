64ada5213a0b879ff13cad396d215d00309acd2a
Buddhadebk <jeetkoner36@gmail.com>
2023-11-12 23:38:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function myresume() {
+    window.open('catagory/resume.html', '_blank');
+}
+function aboutme() {
+    window.open('catagory/about.html', '_blank');
+}
+function project() {
+    window.open('catagory/project.html', '_blank');
+}
+function contactme() {
+    window.open('catagory/contact.html', '_blank');
+}
+function explore() {
+    window.open('catagory/explore.html', '_blank');
+}
+function webdev() {
+    window.open('catagory/webdeb.html', '_blank');
+}
+function colab() {
+    window.open('catagory/colab.html', '_blank');
+}
